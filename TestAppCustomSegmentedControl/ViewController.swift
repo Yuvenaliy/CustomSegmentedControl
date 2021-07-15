ecfc04985fb5ba986a8d5b696624e32873a3e0b5
YuvaKhlopkov <yuvakhlopkov93@gmail.com>
2021-07-15 15:09:40 +0300

    Initial Commit

---
+++ TestAppCustomSegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAppCustomSegmentedControl
+//
+//  Created by Ювеналий on 15.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
